0e852df1fba85eb51ce6ed84432977e86b37ce09
Daniel <daniel.guirra777@gmail.com>
2022-08-06 18:05:57 -0300

    version 1.2.11

---
+++ indexs.js
@@ -0,0 +1,44 @@
+import setupApp from './app.js'
+const port = 3000
+
+;(async () => {
+  try {
+    /**
+     * promise that initializes the database and configures the application
+     */
+    const app = await setupApp()
+    /**
+     * initialize the server on port
+     */
+    const server = app.listen(port, () =>
+      console.info(`⚡| Server is running on port ${port}!`),
+    )
+    /**
+     * @params SIGINT   system interruption
+     * @params SIGTERM  system termination (commonly used)
+     * @params SIGQUIT  abandonment, forced termination
+     */
+    const exitSignals = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+    exitSignals.map((sig) =>
+      process.on(sig, () =>
+        server.close((err) => {
+          if (err) {
+            console.error(err)
+            process.exit(1)
+          }
+          app.database.connection.close(function () {
+            close.info('⛔| Database connection closed!')
+            process.exit(0)
+          })
+        }),
+      ),
+    )
+  } catch (error) {
+    if (error) {
+      console.error(error)
+      process.exit(1)
+    } else {
+      return
+    }
+  }
+})()
